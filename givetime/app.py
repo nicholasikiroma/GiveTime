a8a1fe2fddc2a9add91f540d0e2986740178426d
Nicholas <nicholasikiroma@gmail.com>
2023-02-28 16:42:36 +0100

    added endpoints

--- givetime/app.py
+++ givetime/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Base Application for GiveTime"""
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask('__name__')
@@ -8,7 +9,18 @@ app = Flask('__name__')
 
 @app.route('/')
 def index():
-    return "GiveTime"
+    """Renders template for home page"""
+    return render_template('index.html')
+
+@app.route('/explore')
+def explore():
+    """Renders the html template for the explore page"""
+    return render_template('explore.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
